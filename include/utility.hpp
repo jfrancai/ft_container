1895615b5ca7f0902d1e1d8cb36adcbfb5c3e61d
Julien FRANCAIS <jfrancai@student.42.fr>
2023-02-15 18:26:23 +0100

    adding make pair into ft namespace

---
+++ include/utility.hpp
@@ -0,0 +1,49 @@
+#ifndef UTILITY
+#define UTILITY
+#include <iostream>
+
+namespace ft {
+
+template < class T1, class T2>
+struct pair
+{
+	typedef T1 first_type;
+	typedef T2 second_type;
+
+	first_type first;
+	second_type second;
+
+	pair(void) : first(T1()), second(T2()){}
+	pair(const T1 &x, const T2 &y) : first(x), second(y) {}
+	template< class U1, class U2 >
+	pair(const pair< U1, U2 > &p) : first(p.first), second(p.second) {}
+	pair(const pair &p) : first(p.first), second(p.second) {}
+
+	pair	&operator=(const pair &other) {
+		first = other.first;
+		second = other.second;
+		return (*this);
+	}
+};
+
+template < class T1, class T2, class U1, class U2>
+bool	operator==(const ft::pair< T1, T2 > &lhs, const ft::pair< U1, U2 > &rhs) { return (lhs.first == rhs.first && rhs.second == rhs.second); }
+
+template < class T1, class T2, class U1, class U2>
+bool	operator!=(const ft::pair< T1, T2 > &lhs, const ft::pair< U1, U2 > &rhs) { return (!(lhs == rhs)); }
+
+template < class T1, class T2, class U1, class U2>
+bool	operator<(const ft::pair< T1, T2 > &lhs, const ft::pair< U1, U2 > &rhs) { return (lhs.first < rhs.first || (lhs.first == rhs.first && lhs.second < rhs.second)); }
+
+template < class T1, class T2, class U1, class U2>
+bool	operator>(const ft::pair< T1, T2 > &lhs, const ft::pair< U1, U2 > &rhs) { return (rhs < lhs); }
+
+template < class T1, class T2, class U1, class U2>
+bool	operator<=(const ft::pair< T1, T2 > &lhs, const ft::pair< U1, U2 > &rhs) { return (!(rhs < lhs)); }
+
+template < class T1, class T2, class U1, class U2>
+bool	operator>=(const ft::pair< T1, T2 > &lhs, const ft::pair< U1, U2 > &rhs) { return (!(lhs < rhs)); }
+
+} // namespace ft
+
+#endif
